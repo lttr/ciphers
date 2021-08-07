a094c7be0ffe8bcd9240f993623c4a1c8bc2eb48
Lukas Trumm <lukas.trumm@gmail.com>
2021-08-07 22:09:34 +0200

    Try material navigation component

--- apps/multi-encryptor/src/app/app.module.ts
+++ apps/multi-encryptor/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
+  declarations: [AppComponent, NavigationComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })

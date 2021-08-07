307479b29187a7479eed898e7e4e5ba66a5ffbd5
Lukas Trumm <lukas.trumm@gmail.com>
2021-08-07 21:10:11 +0200

    Test material design component

--- apps/multi-encryptor/src/app/app.module.ts
+++ apps/multi-encryptor/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
